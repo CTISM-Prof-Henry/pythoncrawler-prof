26610c12b602b05161403f4f89bba01ed4f380ce
henryzord <henry.cagnini@gmail.com>
2022-10-15 19:40:44 -0300

    added code for generating graph

--- gera_gráfico.py
+++ gera_gráfico.py
@@ -4,45 +4,61 @@
 from matplotlib import pyplot as plt
 import numpy as np
 import sqlite3
+from datetime import datetime as dt
 
 
 def acessa_banco():
-    con = sqlite3.connect('banco.db')
-    cur = con.cursor()
 
-    dados1 = cur.execute('''
-        select x, y
-        from exemplo 
-        where semestre='primeiro semestre'
-    ''').fetchall()
-    dados2 = cur.execute('''
-        select x, y
-        from exemplo 
-        where semestre='segundo semestre'
-    ''').fetchall()
+    with sqlite3.connect('banco.db') as con:
+        cur = con.cursor()
 
-    # zip(*dados1) transforma uma lista de tuplas em duas tuplas:
-    # dados1 = [(0, 1), (0, 0), (0, 0), (0, 0), (0, 0)]
-    # zip(*dados1) vira x = (0, 0, 0, 0, 0) e y1 = (1, 0, 0, 0, 0)
-    x, y1 = zip(*dados1)
-    x, y2 = zip(*dados2)
+        # TODO espero que vocês tenham prestado atenção nas aulas de banco de dados
+        # TODO recomendo abrir o banco de dados pela linha de comando para rodar esses comandos:
+        # TODO sqlite3 banco.db
+        # TODO CTRL + D fecha o programa
+        dados1 = cur.execute('''
+            select A.id_produto, A.nome, B.preco, B.date_annotation
+            from produtos as A
+            inner join anotacoes_para_produtos as C on A.id_produto = C.id_produto
+            inner join anotacoes as B on C.id_anotacao = B.id_anotacao
+        ''').fetchall()  # type: list
 
-    return list(x), list(y1), list(y2)
+        processados = dict()
+
+        # linha é uma tupla de 4 posições nessa ordem: id_produto, nome, preco, date_annotation
+        # pois essas foram as colunas selecionadas na cláusula select
+        for linha in dados1:
+            # o formato deve ser o mesmo que foi usado para armazenar a string
+            data = dt.strptime(linha[3], '%Y-%m-%d-%H-%M-%S')
+
+            if linha[1] in processados:
+                # adiciona à lista
+                processados[linha[1]] += [{'preço': linha[2], 'data': data}]
+            else:
+                # cria lista
+                processados[linha[1]] = [{'preço': linha[2], 'data': data}]
+
+    return processados
 
 
-def desenha(x, y1, y2):
+def desenha(produtos):
     fig, ax = plt.subplots()
 
-    ax.bar(x, y1, label='primeiro semestre', color='#b434eb')
-    ax.bar(x, y2, label='segundo semestre', color='#86d96c')
+    # existem diversas maneiras de processar os dados. eu optei por fazer assim
+    # não é necessariamente a mais fácil!
+    for nome, dados in produtos.items():
+        datas = []
+        precos = []
+        for linha in dados:
+            datas += [linha['data']]
+            precos += [linha['preço']]
 
-    ax.set_xticks(x)
-    ax.set_xticklabels(['segunda', 'terça', 'quarta', 'quinta', 'sexta'])
+        processados = list(sorted(zip(datas, precos), key=lambda x: x[0]))
+        datas, precos = zip(*processados)
+        datas = [x.timestamp() for x in datas]
+        ax.plot(datas, precos, label=nome)
 
-    ax.set_xlabel('dias da semana')
-    ax.set_ylabel('RAPAAAAAZ')
-
-    ax.set_title('nível de alegria nesta escola')
+    ax.set_title('preços dos produtos coletados pelo crawler')
 
     plt.legend(loc='upper right')
 
@@ -51,8 +67,8 @@ def desenha(x, y1, y2):
 
 
 def main():
-    x, y1, y2 = acessa_banco()
-    desenha(x, y1, y2)
+    produtos = acessa_banco()
+    desenha(produtos)
 
 
 if __name__ == '__main__':
